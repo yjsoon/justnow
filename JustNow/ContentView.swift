d06c196148b0684d6f991fb1322a5cf002c61c67
YJ Soon <yjsoon@tinkertanker.com>
2026-01-03 21:29:45 +0800

    Initial commit

---
+++ JustNow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JustNow
+//
+//  Created by YJ Soon on 3/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JustNow/JustNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustNowApp.swift
+//  JustNow
+//
+//  Created by YJ Soon on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct JustNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
