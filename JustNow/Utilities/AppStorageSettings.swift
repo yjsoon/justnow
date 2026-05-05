5ded94414642c5229d6a51d00054f1c07d63c6a9
YJ Soon <yjsoon@tinkertanker.com>
2026-05-05 11:38:39 +0800

    fix: opt app storage constants out of actor isolation

--- JustNow/Utilities/AppStorageSettings.swift
+++ JustNow/Utilities/AppStorageSettings.swift
@@ -1,46 +1,46 @@
 import Foundation
 
 enum AppStorageKey {
-    static let captureInterval = "captureInterval"
-    static let rewindHistorySeconds = "rewindHistorySeconds"
-    static let recentTimelineWindowSeconds = "recentTimelineWindowSeconds"
-    static let reduceCaptureOnBattery = "reduceCaptureOnBattery"
-    static let shortcutKeyCode = "shortcutKeyCode"
-    static let shortcutModifiers = "shortcutModifiers"
-    static let capturePauseShortcutKeyCode = "capturePauseShortcutKeyCode"
-    static let capturePauseShortcutModifiers = "capturePauseShortcutModifiers"
-    static let overlayDismissKeyCode = "overlayDismissKeyCode"
-    static let overlayDismissModifiers = "overlayDismissModifiers"
-    static let textGrabSoundEnabled = "textGrabSoundEnabled"
-    static let saveScreenshotSoundEnabled = "saveScreenshotSoundEnabled"
-    static let textGrabDebugPreviewEnabled = "textGrabDebugPreviewEnabled"
-    static let showMenuBarIcon = "showMenuBarIcon"
-    static let hasSeenMenuBarHideInfo = "hasSeenMenuBarHideInfo"
-    static let screenshotSaveLocationOverride = "screenshotSaveLocationOverride"
-    static let screenshotSaveToFolder = "screenshotSaveToFolder"
-    static let screenshotSaveToClipboard = "screenshotSaveToClipboard"
-    static let hasSeenSaveQualityInfo = "hasSeenSaveQualityInfo"
-    static let regionScreenshotShortcutHintCount = "regionScreenshotShortcutHintCount"
+    nonisolated static let captureInterval = "captureInterval"
+    nonisolated static let rewindHistorySeconds = "rewindHistorySeconds"
+    nonisolated static let recentTimelineWindowSeconds = "recentTimelineWindowSeconds"
+    nonisolated static let reduceCaptureOnBattery = "reduceCaptureOnBattery"
+    nonisolated static let shortcutKeyCode = "shortcutKeyCode"
+    nonisolated static let shortcutModifiers = "shortcutModifiers"
+    nonisolated static let capturePauseShortcutKeyCode = "capturePauseShortcutKeyCode"
+    nonisolated static let capturePauseShortcutModifiers = "capturePauseShortcutModifiers"
+    nonisolated static let overlayDismissKeyCode = "overlayDismissKeyCode"
+    nonisolated static let overlayDismissModifiers = "overlayDismissModifiers"
+    nonisolated static let textGrabSoundEnabled = "textGrabSoundEnabled"
+    nonisolated static let saveScreenshotSoundEnabled = "saveScreenshotSoundEnabled"
+    nonisolated static let textGrabDebugPreviewEnabled = "textGrabDebugPreviewEnabled"
+    nonisolated static let showMenuBarIcon = "showMenuBarIcon"
+    nonisolated static let hasSeenMenuBarHideInfo = "hasSeenMenuBarHideInfo"
+    nonisolated static let screenshotSaveLocationOverride = "screenshotSaveLocationOverride"
+    nonisolated static let screenshotSaveToFolder = "screenshotSaveToFolder"
+    nonisolated static let screenshotSaveToClipboard = "screenshotSaveToClipboard"
+    nonisolated static let hasSeenSaveQualityInfo = "hasSeenSaveQualityInfo"
+    nonisolated static let regionScreenshotShortcutHintCount = "regionScreenshotShortcutHintCount"
 }
 
 enum AppStorageDefault {
-    static let captureInterval = 0.5
-    static let rewindHistorySeconds = RewindHistoryOption.defaultValue.rawValue
-    static let recentTimelineWindowSeconds = RecentTimelineWindow.defaultValue.rawValue
-    static let reduceCaptureOnBattery = true
-    static let shortcutKeyCode = 38  // J key
-    static let shortcutModifiers = 1_572_864  // ⌘⌥
-    static let capturePauseShortcutKeyCode = 38  // J key
-    static let capturePauseShortcutModifiers = 1_703_936  // ⌘⌥⇧
-    static let overlayDismissKeyCode = 53
-    static let overlayDismissModifiers = 0
-    static let textGrabSoundEnabled = true
-    static let saveScreenshotSoundEnabled = true
-    static let textGrabDebugPreviewEnabled = false
-    static let showMenuBarIcon = true
-    static let hasSeenMenuBarHideInfo = false
-    static let screenshotSaveLocationOverride = ""
-    static let screenshotSaveToFolder = true
-    static let screenshotSaveToClipboard = false
-    static let hasSeenSaveQualityInfo = false
+    nonisolated static let captureInterval = 0.5
+    nonisolated static let rewindHistorySeconds = RewindHistoryOption.defaultValue.rawValue
+    nonisolated static let recentTimelineWindowSeconds = RecentTimelineWindow.defaultValue.rawValue
+    nonisolated static let reduceCaptureOnBattery = true
+    nonisolated static let shortcutKeyCode = 38  // J key
+    nonisolated static let shortcutModifiers = 1_572_864  // ⌘⌥
+    nonisolated static let capturePauseShortcutKeyCode = 38  // J key
+    nonisolated static let capturePauseShortcutModifiers = 1_703_936  // ⌘⌥⇧
+    nonisolated static let overlayDismissKeyCode = 53
+    nonisolated static let overlayDismissModifiers = 0
+    nonisolated static let textGrabSoundEnabled = true
+    nonisolated static let saveScreenshotSoundEnabled = true
+    nonisolated static let textGrabDebugPreviewEnabled = false
+    nonisolated static let showMenuBarIcon = true
+    nonisolated static let hasSeenMenuBarHideInfo = false
+    nonisolated static let screenshotSaveLocationOverride = ""
+    nonisolated static let screenshotSaveToFolder = true
+    nonisolated static let screenshotSaveToClipboard = false
+    nonisolated static let hasSeenSaveQualityInfo = false
 }
